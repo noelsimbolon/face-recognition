fix & style: Fix covariance in eigen.py

src/eigen.py
@@ -1,37 +1,45 @@
 import cv2
 import numpy as np
 import os
-from concurrent.futures import ThreadPoolExecutor
+from math import sqrt
 
 
-def get_covariance(filepath):
+def get_covariance_dir(filepath) -> np.array:
     files = []
     for folder in os.listdir(filepath):
         folder_path = os.path.join(filepath, folder)
         for idx, img in zip(range(10), os.listdir(folder_path)):
             img_path = os.path.join(folder_path, img)
             files.append(img_path)
+    cov = np.array([get_covariance_image(i) for i in files])
+    return cov
+
+
+def get_image_mean(file):
     # langkah 1
-    # with Pool(processes=os.cpu_count()) as pool:
-    #     img = pool.map(cv2.imread, files, chunksize=32)
-    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-        img = executor.map(cv2.imread, files, chunksize=32)
-    # img = [cv2.imread(file) for file in files]
-    img = [cv2.resize(i, (256, 256)) for i in img]
-    img = [i.flatten() for i in img]
-    img = np.array(img, dtype=np.float32)
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (256, 256))
+    img = np.array(img, dtype=np.float64)
 
     # langkah 2
     mean = np.mean(img)
+
     # langkah 3
-    img = img - mean
+    img -= mean
+    return img
+
+
+def get_covariance_image(file):
     # langkah 4
-    cov = np.cov(img)
-    return cov
+    return np.cov(get_image_mean(file))
+
+
+def norm(matrix):
+    return sqrt(sum([i ** 2 for i in matrix]))
+
 
 def qr_decom(matrix):
-    k = np.array(matrix)
-    k = k.astype('float64')
+    k = np.array(matrix, dtype=np.float64)
     c = np.copy(k)
     m, n = k.shape
 
@@ -40,7 +48,7 @@ def qr_decom(matrix):
     for i in range(m):
         a = k[:, i]
 
-        if (i > 0):
+        if i > 0:
             for j in range(i):
                 prev = Q[:, j]
                 temp = k[:, i]
@@ -50,10 +58,10 @@ def qr_decom(matrix):
 
     R = np.matmul(np.transpose(Q), c)
 
-    return Q, R
+    return -Q, -R
 
 
-def eigen(matrix, iteration):
+def eigen(matrix, iteration=100):
     m = np.array(matrix)
     n = m.shape[0]
     eigenvector = np.identity(n)
@@ -82,6 +90,7 @@ def eigen(matrix, iteration):
 
     return eigenvalue, eigenvector
 
+
 def eface(eigenvector, avrmatrix):
     eigenface = list()
     n = avrmatrix.shape[0]
@@ -90,23 +99,25 @@ def eface(eigenvector, avrmatrix):
 
     return eigenface
 
-# buat testing
-A = np.random.rand(20, 20)
 
-inp = np.loadtxt('matrix.txt')
+if __name__ == "__main__":
+    # buat testing
+    A = np.random.rand(20, 20)
 
-e = np.linalg.eig(inp)
-eval = e[0]
-evec = e[1]
+    inp = np.loadtxt('matrix.txt')
 
-eval = np.around(eval, decimals=3)
-evec = np.around(evec, decimals=3)
+    e = np.linalg.eigh(inp)
+    eval = e[0]
+    evec = e[1]
 
-print(eval)
-print(evec)
+    eval = np.around(eval, decimals=3)
+    evec = np.around(evec, decimals=3)
 
-result = eigen(inp, 100000) #iterationnya bebas diganti berapa
-""" np.savetxt('eigval.txt',result[0])
-np.savetxt('eigvec.txt',result[1]) """
-print(result[0])
-print(result[1])
+    print(eval)
+    print(evec)
+
+    result = eigen(inp, 100000)  # iterationnya bebas diganti berapa
+    """ np.savetxt('eigval.txt',result[0])
+    np.savetxt('eigvec.txt',result[1]) """
+    print(result[0])
+    print(result[1])